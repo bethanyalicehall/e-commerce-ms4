db92312146ee59a89eb05a5542d5c62ac993ce88
bethanyalicehall <bethany.hall66@gmail.com>
2023-08-16 16:27:25 +0000

    Added cache control

--- shop/settings.py
+++ shop/settings.py
@@ -209,13 +209,18 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'monstera-and-more'
     AWS_S3_REGION_NAME = 'eu-west-2' 
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
