3f406e87e2a3ee919001f3dbd6bee72814545478
bethanyalicehall <bethany.hall66@gmail.com>
2023-08-16 12:36:08 +0000

    Re-added products and began deployment

--- shop/settings.py
+++ shop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-))sle1%d8l&^aq4904&t&qq%&we#7mujzwn4$zz6or9^jo-1d$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '8000-bethanyalic-ecommercems-xl58jfc9t62.ws-eu103.gitpod.io', '8000-bethanyalic-ecommercems-xl58jfc9t62.ws-eu101.gitpod.io', '8000-bethanyalic-ecommercems-6upl60xiz36.ws-eu100.gitpod.io']
+ALLOWED_HOSTS = ['localhost', 'monstera-and-more.herokuapp.com', '8000-bethanyalic-ecommercems-xl58jfc9t62.ws-eu103.gitpod.io', '8000-bethanyalic-ecommercems-xl58jfc9t62.ws-eu101.gitpod.io', '8000-bethanyalic-ecommercems-6upl60xiz36.ws-eu100.gitpod.io']
 
 
 # Application definition
@@ -124,7 +125,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -136,13 +137,24 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+   
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
