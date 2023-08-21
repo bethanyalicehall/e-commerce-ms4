8bd0bb8635a9d393d11be86a7d5782df450bcbcc
bethanyalicehall <bethany.hall66@gmail.com>
2023-08-21 11:23:49 +0000

    Temporarily changed x frame options

--- shop/settings.py
+++ shop/settings.py
@@ -64,7 +64,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
+
+X_FRAME_OPTIONS = 'ALLOWALL'
 
 ROOT_URLCONF = 'shop.urls'
 
